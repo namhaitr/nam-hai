869089547d93cd340668dc9c513143d3a7232d42
namhaitr <105618084+namhaitr@users.noreply.github.com>
2022-05-17 08:47:51 +0700

    pt bac 2

---
+++ data c.c
@@ -0,0 +1,12 @@
+#include,stdio.h>//input thu vien va xuat cua c
+int main (){
+short a ;
+	long b;
+long double c;
+float x; 
+char ch ;
+int i ;
+//ham in ra man hinh : printf("");
+printf("\n kich thuoc cua short = %d",sizeof(a));
+printf("\n kich thuoc cua long = %d",sizeo(a));
+
