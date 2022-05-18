2c0991d13ffdb717b873d3ec2d94aeafa2805acc
namhaitr <105618084+namhaitr@users.noreply.github.com>
2022-05-18 22:11:38 +0700

    Create bt1.cpp
    bt 1

---
+++ bt1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+	int n, i;
+	do
+	{
+	printf("nhap N : ");
+	scanf("%u" , &n);
+}while(n<=0);
+for(i = 1 ; i<=n ; i++)
+{
+	if(i%2 == 0)
+	continue;
+	printf("%u" ,i);
+}
+}
+
+
+
+
+
