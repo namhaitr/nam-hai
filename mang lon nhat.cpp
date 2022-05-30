ea3cf7dd59ee5b946c341aff8bb6acb5a0018d8f
namhaitr <105618084+namhaitr@users.noreply.github.com>
2022-05-30 21:00:37 +0700

    Create mang lon nhat.cpp
    mảng

---
+++ mang lon nhat.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int ary[10];
+	int i , total , high ;
+	for (i=0; i<10; i++)
+{
+	scanf("%d" , &ary [i]);
+}
+/* Displays highest of the etered values */
+high = ary [0];
+for (i=1; i<10; i ++)
+{
+	if(ary[i] > high)
+high = ary[i] ;
+}
+printf ("\nHighest value entered was %d" , high);
+
+
+for(int i = 1 ; i < 10 ; i++){
+	if (max == mang[i]);
+	{
+		continue;
+	}
+	else if (max 2 mang [i]){
+		max2 = mang[i];
+	}
+}
+// gan gia tri lon thu hai cua mang cho max 2
+printf("output:%d,max2")
+	
+	
+}
